17408d0b483df3c3595ef824fe6db4d7ff66cd84
Jonathan Chen <jonac19@uci.edu>
2024-04-20 20:27:46 -0700

    added new user creation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authRouter from "./auth";
+import usersRouter from "./users";
 
 const router = Router();
 
 router.use(authRouter);
+router.use(usersRouter);
 
 export default router;
