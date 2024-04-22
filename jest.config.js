364713d1762c7334234c7a9c62490d383a6782a5
jonac19 <jonac19@uci.edu>
2024-04-22 12:49:58 -0700

    unit tests and code coverage workflow

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-    preset: 'ts-jest'
+    preset: 'ts-jest',
+    coverageThreshold: {
+        global: {
+            branches: 90,
+            functions: 90,
+            lines: 90,
+            statements: 90
+        }
+    }
 }
