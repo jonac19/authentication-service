84ae198b00e192fad3b54b367542d8c2cd7dd92a
Jonathan Chen <jonac19@uci.edu>
2024-04-21 23:21:19 -0700

    separated out controller logic

---
+++ src/controllers/authController.ts
@@ -0,0 +1,31 @@
+import { compare } from 'bcrypt';
+import pool from '../database/pool'
+
+export async function authController(req: any, res: any) {
+    if (!req.body.username) {
+        return res.status(400).json({ error: "Username is missing" });
+    }
+
+    if (!req.body.password) {
+        return res.status(400).json({ error: "Password is missing" });
+    }
+
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT * FROM users WHERE username = $1', [req.body.username]);
+        client.release();
+        
+        if (!result.rows.length) {
+            return res.status(400).json({ error: "Username doesn't exist" });
+        }
+
+        if (!await compare(req.body.password, result.rows[0].password)) {
+            return res.status(400).json({ error: "Password incorrect" });
+        }
+
+        res.status(200).json({ success: "Successfully authenticated" });
+    } catch (error) {
+        console.error(`Error executing query: ${error}`);
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,36 +1,8 @@
-import { Router } from 'express';
-import { compare } from 'bcrypt';
-import pool from '../database/pool'
+import { Router, Request, Response } from 'express';
+import { authController } from '../controllers/authController';
 
 const router = Router();
 
-router.post('/api/auth', async (req, res) => {
-    if (!req.body.username) {
-        return res.status(400).json({ error: "Username is missing" });
-    }
-
-    if (!req.body.password) {
-        return res.status(400).json({ error: "Password is missing" });
-    }
-
-    try {
-        const client = await pool.connect();
-        const result = await client.query('SELECT * FROM users WHERE username = $1', [req.body.username]);
-        client.release();
-        
-        if (!result.rows.length) {
-            return res.status(400).json({ error: "Username doesn't exist" });
-        }
-
-        if (!await compare(req.body.password, result.rows[0].password)) {
-            return res.status(400).json({ error: "Password incorrect" });
-        }
-
-        res.json({ success: "Successfully authenticated" });
-    } catch (error) {
-        console.error(`Error executing query: ${error}`);
-        res.status(500).json({ error: "Internal server error" });
-    }
-})
+router.post('/api/auth', authController);
 
 export default router;
