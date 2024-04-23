5f779fd8a2d7780c07b506f6b4f70a5fe9e859ed
jonac19 <jonac19@uci.edu>
2024-04-22 19:07:02 -0700

    clean up config

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,6 @@ import type {Config} from 'jest';
 
 const config: Config = {
     preset: 'ts-jest',
-    coverageReporters: ["clover", "json", ["text", { file: 'coverage.txt' }]]
 };
 
 export default config;
