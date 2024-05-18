7a87ca13077a19d19a4a8711eb90a0c3417580c8
Jonathan Chen <jonac19@uci.edu>
2024-05-17 22:40:09 -0700

    display artist image

--- frontend/src/Artists/Artist.tsx
+++ frontend/src/Artists/Artist.tsx
@@ -1,9 +1,16 @@
 import { ArtistProp } from "../Interfaces/ArtistProp";
+import { ArtistData } from "../Interfaces/ArtistsData";
+
+function getImage(artist: ArtistData) {
+    const image = artist.images.find((image) => image.width == 305 && image.height == 225)
+    return image ? image : artist.images[0]
+}
 
 export default function Artist({ artist }: ArtistProp) {
     return (
         <div>
-            <p>{artist.name}</p>
+            <h3>{artist.name}</h3>
+            <img src={getImage(artist).url}></img>
         </div>
     )
 }
--- frontend/src/Interfaces/ArtistsData.tsx
+++ frontend/src/Interfaces/ArtistsData.tsx
@@ -1,12 +1,20 @@
+export type ArtistsData = {
+    _embedded: {
+        attractions: ArtistData[]
+    }
+}
+
 export type ArtistData = {
     name: string,
     id: string,
     url: string,
     locale: string
+    images: Image[]
 }
 
-export type ArtistsData = {
-    _embedded: {
-        attractions: ArtistData[]
-    }
+type Image = {
+    ratio: string,
+    url: string,
+    width: number,
+    height: number
 }
