5d93b31aa8b4b1dc55c7a4e15df39c36a535cc26
Jonathan Chen <jonac19@uci.edu>
2024-05-24 21:55:40 -0700

    added dependency injection

--- backend/src/controllers/ticketmasterController.ts
+++ backend/src/controllers/ticketmasterController.ts
@@ -1,12 +1,12 @@
-import axios from 'axios';
 import fs from 'fs';
 import path from 'path';
+import axios from 'axios';
 
 const TicketmasterClient = axios.create({
     baseURL: "https://app.ticketmaster.com/discovery/v2/"
 });
 
-export async function getArtists(req: any, res: any) {
+export async function getArtists(req: any, res: any, client = TicketmasterClient) {
     if (process.env.DEV_MODE == "dev") {
         fs.readFile(path.resolve(__dirname, './../__tests__/artists.json'), 'utf-8', (err, data) => {
             res.send(JSON.parse(data));
@@ -21,33 +21,29 @@ export async function getArtists(req: any, res: any) {
             classificationId: process.env.TM_ARTIST_CLASS_ID
         };
 
-        const response = await TicketmasterClient.get("attractions", {
+        const response = await client.get("attractions", {
             params: params
         });
         
-        res.send(response.data);
+        res.status(200).json(response.data);
     } catch (error) {
         console.error("Error querying Ticketmaster for artists");
         res.status(500).json({ msg: "Internal server error" });
     }
 }
 
-export async function getArtist(req: any, res: any) {
+export async function getArtist(req: any, res: any, client = TicketmasterClient) {
     try {
-        if (!req.params.artistId) {
-            return res.status(400).json({ msg: "Artist ID is missing" });
-        }
-
         const params = {
             apikey: process.env.TM_KEY,
             locale: "*"
         };
 
-        const response = await TicketmasterClient.get(`attractions/${req.params.artistId}`, {
+        const response = await client.get(`attractions/${req.params.artistId}`, {
             params: params
         });
     
-        res.send(response.data);
+        res.status(200).json(response.data);
     } catch (error) {
         console.error("Error querying Ticketmaster for artist");
         res.status(500).json({ msg: "Internal server error" });
--- backend/src/routes/ticketmaster.ts
+++ backend/src/routes/ticketmaster.ts
@@ -3,7 +3,7 @@ import * as TicketmasterController from '../controllers/ticketmasterController';
 
 const router = Router();
 
-router.get('/api/artists', TicketmasterController.getArtists);
-router.get('/api/artists/:artistId', TicketmasterController.getArtist);
+router.get('/api/artists', (req, res) => TicketmasterController.getArtists(req, res));
+router.get('/api/artists/:artistId', (req, res) => TicketmasterController.getArtist(req, res));
 
 export default router;
