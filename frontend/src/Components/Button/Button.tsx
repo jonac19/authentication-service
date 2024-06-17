49d225216da0012c3b98d1d219644e82465649d7
Jonathan Chen <jonac19@uci.edu>
2024-06-16 20:50:58 -0700

    refactor: moved ButtonProp into Button

--- frontend/src/Components/Button/Button.tsx
+++ frontend/src/Components/Button/Button.tsx
@@ -1,8 +1,11 @@
-import { ButtonProp } from '../../data/ButtonProp'
 import './Button.css'
 
 export default function Button({ label }: ButtonProp) {
     return (
         <button className='button'>{label}</button>
     )
+}
+
+interface ButtonProp {
+    label: string
 }
