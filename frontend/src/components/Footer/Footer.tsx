3cce15039f12757870e61bbe3b2009387f595d45
Jonathan Chen <jonac19@uci.edu>
2024-06-17 15:56:20 -0700

    feat: added and styled footer

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import './Footer.css'
+
+export default function Footer() {
+    return (
+        <div className='footer'>
+            <h5>@2024 ShowFlow. All rights reserved - Terms and Conditions - Privacy Policy - Website by Jonathan</h5>
+        </div>
+    )
+}
--- frontend/src/pages/Home/HomePage.tsx
+++ frontend/src/pages/Home/HomePage.tsx
@@ -2,6 +2,7 @@ import Header from '../../components/Header/Header'
 import Hero from '../../components/Hero/Hero'
 import SearchBar from '../../components/SearchBar/SearchBar'
 import ArtistsSection from './Artists/ArtistsSection'
+import Footer from '../../components/Footer/Footer'
 
 
 export default function HomePage() {
@@ -11,6 +12,7 @@ export default function HomePage() {
             <Hero />
             <SearchBar placeholder='Search by Artist'/>
             <ArtistsSection />
+            <Footer />
         </div>
     )
 }
