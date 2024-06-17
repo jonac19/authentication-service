23acd4d69e845abd943a29a925302df596e467b8
Jonathan Chen <jonac19@uci.edu>
2024-06-16 20:28:31 -0700

    feat: added a homepage component

---
+++ frontend/src/pages/Home/HomePage.tsx
@@ -0,0 +1,14 @@
+import Header from '../../components/Header/Header'
+import Hero from '../../components/Hero/Hero'
+import ArtistsBody from './Artists/ArtistsBody'
+
+
+export default function HomePage() {
+    return (
+        <div>
+            <Header />
+            <Hero />
+            <ArtistsBody />
+        </div>
+    )
+}
