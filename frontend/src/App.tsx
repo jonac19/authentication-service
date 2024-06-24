d28e391783daf22b79f2e6a1956541bcb831e5d0
Jonathan Chen <jonac19@uci.edu>
2024-06-24 14:56:55 -0700

    feat: manage routes with react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,20 @@
 import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/Login/LoginPage'
 import HomePage from './pages/Home/HomePage'
 
 function App() {
   const [token, setToken] = useState<string>();
 
-  if (!token) {
-    return <LoginPage setToken={setToken} />
-  }
-
   return (
-    <div className='App'>
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/'>
+          <Route index element={<HomePage />} />
+          <Route path='login' element={<LoginPage setToken={setToken} />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
