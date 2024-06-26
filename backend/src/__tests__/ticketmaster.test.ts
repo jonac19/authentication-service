76d457510b4799b9d92a4ceaa3f9f84e8475a3f2
Jonathan Chen <jonac19@uci.edu>
2024-06-26 16:14:52 -0700

    feat: added tests for show routes

--- backend/src/__tests__/ticketmaster.test.ts
+++ backend/src/__tests__/ticketmaster.test.ts
@@ -1,13 +1,19 @@
-import { getArtists, getArtist } from '../controllers/ticketmasterController';
+import { getArtists, getArtist, getShows, getShowsByArtist } from '../controllers/ticketmasterController';
 
-jest.mock('axios', () => ({
-    create: jest.fn()
-}));
+jest.mock('axios', () => {
+    const mockedClient: any = {
+        get: jest.fn(() => ({
+            data: "test_data"
+        }))
+    };
 
-const mockedClient: any = {
-    get: jest.fn(() => ({
-        data: "test_data"
-    }))
+    return ({
+        create: jest.fn(() => mockedClient)
+    })
+});
+
+const errorMockedClient: any = {
+    get: jest.fn(() => {throw Error()})
 };
 
 const mockedResponse = {
@@ -20,21 +26,13 @@ describe('Tests for ticketmaster api - getArtists', () => {
         jest.clearAllMocks();
     });
 
-    beforeEach(() => {
-        process.env.DEV_MODE = 'test';
-    });
-
     it ('should send 200 status code when data successfully retrieved', async () => {
-        await getArtists({}, mockedResponse, mockedClient);
+        await getArtists({}, mockedResponse);
         expect(mockedResponse.status).toHaveBeenCalledWith(200);
         expect(mockedResponse.json).toHaveBeenCalled();
     });
 
     it ('should send 500 status code when error is thrown', async () => {
-        const errorMockedClient: any = {
-            get: jest.fn(() => {throw Error()})
-        };
-        
         await getArtists({}, mockedResponse, errorMockedClient);
         expect(mockedResponse.status).toHaveBeenCalledWith(500);
         expect(mockedResponse.json).toHaveBeenCalled();
@@ -47,28 +45,63 @@ describe('Tests for ticketmaster api - getArtist', () => {
             artistId: 'test_artist_id'
         }
     };
-    
+
     beforeEach(() => {
         jest.clearAllMocks();
     });
 
-    beforeEach(() => {
-        process.env.DEV_MODE = 'test';
-    });
-
     it ('should send 200 status code when data successfully retrieved', async () => {
-        await getArtist(mockedRequest, mockedResponse, mockedClient);
+        await getArtist(mockedRequest, mockedResponse);
         expect(mockedResponse.status).toHaveBeenCalledWith(200);
         expect(mockedResponse.json).toHaveBeenCalled();
     });
 
     it ('should send 500 status code when error is thrown', async () => {
-        const errorMockedClient: any = {
-            get: jest.fn(() => {throw Error()})
-        };
-        
         await getArtist({}, mockedResponse, errorMockedClient);
         expect(mockedResponse.status).toHaveBeenCalledWith(500);
         expect(mockedResponse.json).toHaveBeenCalled();
     });
 })
+
+describe('Tests for ticketmaster api - getShows', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it ('should send 200 status code when data successfully retrieved', async () => {
+        await getShows({}, mockedResponse);
+        expect(mockedResponse.status).toHaveBeenCalledWith(200);
+        expect(mockedResponse.json).toHaveBeenCalled();
+    });
+
+    it ('should send 500 status code when error is thrown', async () => {
+        await getShows({}, mockedResponse, errorMockedClient);
+        expect(mockedResponse.status).toHaveBeenCalledWith(500);
+        expect(mockedResponse.json).toHaveBeenCalled();
+    });
+})
+
+describe('Tests for ticketmaster api - getArtist', () => {
+    const mockedRequest = {
+        params: {
+            artistId: 'test_artist_id'
+        }
+    };
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it ('should send 200 status code when data successfully retrieved', async () => {
+        await getShowsByArtist(mockedRequest, mockedResponse);
+        expect(mockedResponse.status).toHaveBeenCalledWith(200);
+        expect(mockedResponse.json).toHaveBeenCalled();
+    });
+
+    it ('should send 500 status code when error is thrown', async () => {
+        await getShowsByArtist({}, mockedResponse, errorMockedClient);
+        expect(mockedResponse.status).toHaveBeenCalledWith(500);
+        expect(mockedResponse.json).toHaveBeenCalled();
+    });
+})
+
